5e46843bea7b00b3bc886cd7c71369fc1a746d1a
Kaz Matthews and Marc Larkin <kaz+marc@>
2017-02-16 15:44:49 -0800

    will count up to 999 and account for 11 - 19

--- Objects/NTW.cs
+++ Objects/NTW.cs
@@ -10,10 +10,11 @@ namespace NTW.Objects
         private string _inputString;
         private List<int> _digits = new List<int>{};
         private int _lastIndex;
+        private int _count;
         private string[] _ones = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         private string[] _teens = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
         private string[] _tens = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-        // private string _hundred = "hundred";
+        private string _hundred = "hundred";
         private List<string> _outputWord = new List<string>{};
 
         public NumToWord (int input)
@@ -25,7 +26,7 @@ namespace NTW.Objects
                 _digits.Add(int.Parse(_inputString[j].ToString()));
             }
             _lastIndex = _digits[_digits.Count - 1];
-            Console.WriteLine(_lastIndex);
+            _count = _digits.Count;
         }
 
         public List<string> Convert()
@@ -35,40 +36,19 @@ namespace NTW.Objects
                 _outputWord.Insert(0, _ones[_lastIndex - 1]);
             }
 
-            if (_digits.Count == 2)
+            if (_digits.Count > 1 && ((_digits[_count - 2] > 1) || ((_digits[_count - 2] == 1) && (_lastIndex == 0))))
             {
-                _outputWord.Insert(0, _tens[_digits[0] - 1]);
-                // if (_digits[0] == 1)
-                // {
-                //     _outputWord = _teens[_digits[1] - 1];
-                // }
-                // else
-                // {
-                    // _outputWord = _tens[_digits[0] - 1];
-                    // if (_digits[1] > 0)
-                    // {
-                    // }
-                // }
+                _outputWord.Insert(0, _tens[_digits[_count - 2] - 1]);
             }
-            // else if (_digits.Count == 3)
-            // {
-            //     _outputWord = _ones[_digits[0] - 1] + " " + _hundred;
-            //     if (_digits[1] == 1)
-            //     {
-            //         _outputWord = _outputWord + " " + _teens[_digits[2]-1];
-            //     }
-            //     else
-            //     {
-            //         if (_digits[1] > 1)
-            //         {
-            //             _outputWord = _outputWord + " " + _tens[_digits[1]-1];
-            //         }
-            //         if (_digits[2] > 0)
-            //         {
-            //             _outputWord = _outputWord + " " + _ones[_digits[2]-1];
-            //         }
-            //     }
-            // }
+            if(_count > 2 && _digits[_count - 3] > 0)
+            {
+                _outputWord.Insert(0, _ones[_digits[_count - 3] - 1] + " " + _hundred);
+            }
+            if (_digits.Count >1 && (_digits[_count - 2] == 1) && (_lastIndex > 0))
+            {
+                _outputWord.RemoveAt(_outputWord.Count - 1);
+                _outputWord.Insert(_outputWord.Count, _teens[_digits[_count - 1] - 1]);
+            }
             return _outputWord;
         }
     }
--- Tests/ScrabbleTest.cs
+++ Tests/ScrabbleTest.cs
@@ -6,14 +6,15 @@ namespace NTW.Objects
 {
   public class NumToWordTest
   {
-      // [Fact]
-      // public void InputOne_ReturnOne_One()
-      // {
-      //   int userInput = 1;
-      //   NumToWord newNumToWord = new NumToWord(userInput);
-      //   int userOutput = newNumToWord.Convert();
-      //   Assert.Equal(1, userOutput);
-      // }
+      [Fact]
+      public void InputOne_ReturnOne_One()
+      {
+        int userInput = 1;
+        NumToWord newNumToWord = new NumToWord(userInput);
+        List<string> userOutput = newNumToWord.Convert();
+        List<string> expectedOutput = new List<string> {"one"};
+        Assert.Equal(expectedOutput, userOutput);
+      }
     [Fact]
     public void InputSix_ReturnSixText_Six()
     {
@@ -23,63 +24,68 @@ namespace NTW.Objects
       List<string> expectedOutput = new List<string> {"six"};
       Assert.Equal(expectedOutput, userOutput);
     }
-    // [Fact]
-    // public void InputTeen_ReturnTeenText_Teen()
-    // {
-    //   int userInput = 11;
-    //   NumToWord newNumToWord = new NumToWord(userInput);
-    //   string userOutput = newNumToWord.Convert();
-    //   Assert.Equal("eleven", userOutput);
-    // }
+    [Fact]
+    public void InputTeen_ReturnTeenText_Teen()
+    {
+      int userInput = 15;
+      NumToWord newNumToWord = new NumToWord(userInput);
+      List<string> userOutput = newNumToWord.Convert();
+      List<string> expectedOutput = new List<string> {"fifteen"};
+      Assert.Equal(expectedOutput, userOutput);
+    }
     [Fact]
     public void InputTens_ReturnTensText_Tens()
     {
-      int userInput = 70;
+      int userInput = 10;
       NumToWord newNumToWord = new NumToWord(userInput);
       List<string> userOutput = newNumToWord.Convert();
-      List<string> expectedOutput = new List<string> {"seventy"};
+      List<string> expectedOutput = new List<string> {"ten"};
       Assert.Equal(expectedOutput, userOutput);
     }
     [Fact]
     public void Input21_ReturnTensAndOnesText_TensAndOnes()
     {
-      int userInput = 75;
+      int userInput = 99;
       NumToWord newNumToWord = new NumToWord(userInput);
       List<string> userOutput = newNumToWord.Convert();
-      List<string> expectedOutput = new List<string> {"seventy", "five"};
+      List<string> expectedOutput = new List<string> {"ninety", "nine"};
       Assert.Equal(expectedOutput, userOutput);
     }
-    // [Fact]
-    // public void Input21_ReturnTensAndOnesText_Hundreds()
-    // {
-    //   int userInput = 400;
-    //   NumToWord newNumToWord = new NumToWord(userInput);
-    //   string userOutput = newNumToWord.Convert();
-    //   Assert.Equal("four hundred", userOutput);
-    // }
-    // [Fact]
-    // public void Input21_ReturnTensAndOnesText_OneHUndredTwenty()
-    // {
-    //   int userInput = 120;
-    //   NumToWord newNumToWord = new NumToWord(userInput);
-    //   string userOutput = newNumToWord.Convert();
-    //   Assert.Equal("one hundred twenty", userOutput);
-    // }
-    // [Fact]
-    // public void Input21_ReturnTensAndOnesText_OneHUndredTwentyThree()
-    // {
-    //   int userInput = 999;
-    //   NumToWord newNumToWord = new NumToWord(userInput);
-    //   string userOutput = newNumToWord.Convert();
-    //   Assert.Equal("nine hundred ninety nine", userOutput);
-    // }
-    // [Fact]
-    // public void Input21_ReturnTensAndOnesText_OneHundredPlusTeens()
-    // {
-    //   int userInput = 115;
-    //   NumToWord newNumToWord = new NumToWord(userInput);
-    //   string userOutput = newNumToWord.Convert();
-    //   Assert.Equal("one hundred fifteen", userOutput);
-    // }
+    [Fact]
+    public void Input21_ReturnTensAndOnesText_Hundreds()
+    {
+      int userInput = 400;
+      NumToWord newNumToWord = new NumToWord(userInput);
+      List<string> userOutput = newNumToWord.Convert();
+      List<string> expectedOutput = new List<string> {"four hundred"};
+      Assert.Equal(expectedOutput, userOutput);
+    }
+    [Fact]
+    public void Input21_ReturnTensAndOnesText_OneHUndredTwenty()
+    {
+      int userInput = 120;
+      NumToWord newNumToWord = new NumToWord(userInput);
+      List<string> userOutput = newNumToWord.Convert();
+      List<string> expectedOutput = new List<string> {"one hundred", "twenty"};
+      Assert.Equal(expectedOutput, userOutput);
+    }
+    [Fact]
+    public void Input21_ReturnTensAndOnesText_OneHUndredTwentyThree()
+    {
+      int userInput = 999;
+      NumToWord newNumToWord = new NumToWord(userInput);
+      List<string> userOutput = newNumToWord.Convert();
+      List<string> expectedOutput = new List<string> {"nine hundred", "ninety", "nine"};
+      Assert.Equal(expectedOutput, userOutput);
+    }
+    [Fact]
+    public void Input21_ReturnTensAndOnesText_OneHundredPlusTeens()
+    {
+      int userInput = 115;
+      NumToWord newNumToWord = new NumToWord(userInput);
+      List<string> userOutput = newNumToWord.Convert();
+      List<string> expectedOutput = new List<string> {"one hundred", "fifteen"};
+      Assert.Equal(expectedOutput, userOutput);
+    }
   }
 }
